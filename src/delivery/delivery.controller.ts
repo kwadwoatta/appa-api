ad9f69e8383d99c72165afa49df7c691f0f9a4dd
kwadwoatta <princeofori065@gmail.com>
2024-05-27 08:52:06 +0000

    update delivery module controller and service

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -8,41 +8,59 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { AllowRole, Role } from 'common';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { User } from 'src/user';
 import { DeliveryService } from './delivery.service';
 import { CreateDeliveryDto } from './dto/create-delivery.dto';
 import { UpdateDeliveryDto } from './dto/update-delivery.dto';
 
 @UseGuards(JwtGuard)
-@Controller('delivery')
+@Controller()
 export class DeliveryController {
   constructor(private readonly deliveryService: DeliveryService) {}
 
-  @Post()
+  @AllowRole(Role.Admin)
+  @Post('delivery')
   create(@Body() createDeliveryDto: CreateDeliveryDto) {
     return this.deliveryService.create(createDeliveryDto);
   }
 
-  @Get()
+  @AllowRole(Role.Admin)
+  @Get('delivery')
   findAll() {
     return this.deliveryService.findAll();
   }
 
-  @Get(':id')
+  @AllowRole(Role.Admin)
+  @Get('delivery:id')
   findOne(@Param('id') id: string) {
-    return this.deliveryService.findOne(+id);
+    return this.deliveryService.findOne(id);
   }
 
-  @Patch(':id')
+  @Get('users/me/delivery')
+  findAllForUser(@GetUser() user: User) {
+    return this.deliveryService.findAllForUser(user._id);
+  }
+
+  @Get('users/me/delivery/:id')
+  findOneForUser(@GetUser() user: User, @Param('id') id: string) {
+    return this.deliveryService.findOneForUser(user._id, id);
+  }
+
+  @AllowRole(Role.Admin)
+  @Patch('delivery:id')
   update(
     @Param('id') id: string,
     @Body() updateDeliveryDto: UpdateDeliveryDto,
   ) {
-    return this.deliveryService.update(+id, updateDeliveryDto);
+    return this.deliveryService.update(id, updateDeliveryDto);
   }
 
-  @Delete(':id')
+  @AllowRole(Role.Admin)
+  @Delete('delivery:id')
   remove(@Param('id') id: string) {
-    return this.deliveryService.remove(+id);
+    return this.deliveryService.remove(id);
   }
 }
--- src/delivery/delivery.service.ts
+++ src/delivery/delivery.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateDeliveryDto } from './dto/create-delivery.dto';
 import { UpdateDeliveryDto } from './dto/update-delivery.dto';
+import { Delivery, DeliveryModel } from './entities/delivery.entity';
 
 @Injectable()
 export class DeliveryService {
-  create(createDeliveryDto: CreateDeliveryDto) {
-    return 'This action adds a new delivery';
+  constructor(
+    @InjectModel(Delivery.name)
+    private readonly deliveryModel: typeof DeliveryModel,
+  ) {}
+
+  create(dto: CreateDeliveryDto) {
+    return this.deliveryModel.create(dto);
   }
 
   findAll() {
-    return `This action returns all delivery`;
+    return this.deliveryModel.find().populate(['from_user', 'to_user']).exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} delivery`;
+  findOne(deliveryId: string) {
+    return this.deliveryModel
+      .findOne({
+        _id: deliveryId,
+      })
+      .populate(['from_user', 'to_user'])
+      .exec();
   }
 
-  update(id: number, updateDeliveryDto: UpdateDeliveryDto) {
-    return `This action updates a #${id} delivery`;
+  async findAllForUser(userId: string) {
+    return this.deliveryModel
+      .find({
+        $or: [{ from_user: userId }, { to_user: userId }],
+      })
+      .exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} delivery`;
+  async findOneForUser(userId: string, deliveryId: string) {
+    return this.deliveryModel
+      .findOne({
+        _id: deliveryId,
+        $or: [{ from_user: userId }, { to_user: userId }],
+      })
+      .populate(['from_user', 'to_user'])
+      .exec();
+  }
+
+  update(deliveryId: string, dto: UpdateDeliveryDto) {
+    return this.deliveryModel.findByIdAndUpdate(deliveryId, { ...dto }).exec();
+  }
+
+  remove(id: string) {
+    return this.deliveryModel.findByIdAndDelete(id).exec();
   }
 }
